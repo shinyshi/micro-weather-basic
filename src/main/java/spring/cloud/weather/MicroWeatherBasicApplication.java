9efe6ee68e32de57e58ca96d48d0a2d1a1e1f688
shirx <dcy2804@dingtalk.com>
2019-07-02 20:23:42 +0800

    添加注释

--- src/main/java/spring/cloud/weather/MicroWeatherBasicApplication.java
+++ src/main/java/spring/cloud/weather/MicroWeatherBasicApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+//主方法
 public class MicroWeatherBasicApplication {
 
     public static void main(String[] args) {
