857e77a098dd2c16c6fb6a2e84897310eb4b0f10
shirx <dcy2804@dingtalk.com>
2019-07-03 09:53:25 +0800

    Revert "添加注释"
    This reverts commit 9343eec

--- src/main/java/spring/cloud/weather/MicroWeatherBasicApplication.java
+++ src/main/java/spring/cloud/weather/MicroWeatherBasicApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroWeatherBasicApplication {
 
     public static void main(String[] args) {
-        //
+
         SpringApplication.run(MicroWeatherBasicApplication.class, args);
     }
 
