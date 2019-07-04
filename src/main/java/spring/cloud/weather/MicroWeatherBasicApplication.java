c2078faf70cef2622af6e2dbeabc4533ad6b397c
shirx <dcy2804@dingtalk.com>
2019-07-04 16:09:30 +0800

    修改Eureka client配置

--- src/main/java/spring/cloud/weather/MicroWeatherBasicApplication.java
+++ src/main/java/spring/cloud/weather/MicroWeatherBasicApplication.java
@@ -2,9 +2,10 @@ package spring.cloud.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-//主方法
 public class MicroWeatherBasicApplication {
 
     public static void main(String[] args) {
