cfdc803be724d1a2ee7ad8f94fb1e0bed36da72f
shirx <dcy2804@dingtalk.com>
2019-11-22 16:50:58 +0800

    modify

--- src/main/java/spring/cloud/weather/controller/WeatherController.java
+++ src/main/java/spring/cloud/weather/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package spring.cloud.weather.controller;
 
+import javafx.print.Printer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.cloud.weather.service.IWeatherService;
 import spring.cloud.weather.vo.WeatherResponse;
+
+import java.util.logging.Logger;
 
 
 @RestController
@@ -19,6 +22,7 @@ public class WeatherController {
     @GetMapping("/cityId/{cityId}")
     public WeatherResponse getReportByCityId(@PathVariable("cityId") String cityId)
     {
+        System.out.print("收到请求获取天气信息");
         WeatherResponse res=weatherService.getDataByCityId(cityId);
         res.setDesc(cityId);
         return res;
